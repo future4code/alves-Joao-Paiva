0495e06d6a4facfdfe3dd28eb1eebf06ca37d4c7
João Pedro Paiva da Costa Sousa <paivapedro16.pp@gmail.com>
2022-06-13 19:30:26 -0300

    Não consegui realizar ativação dos botoes

---
+++ aula-de-tudo/git-1/modulo-2/rend-condicional-form/rend-condicional-form-app/src/components/final.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+class Final extends React.Component {
+    render() {
+
+        return (
+          <div className="Etapa3">
+            <p><strong> O FORMULÁRIO ACABOU</strong></p>
+            <label>
+     <li>Muito obrigado por participar! Entraremos em contato!</li>
+     </label>
+        </div> 
+        );
+  }
+}
+export default Final;
