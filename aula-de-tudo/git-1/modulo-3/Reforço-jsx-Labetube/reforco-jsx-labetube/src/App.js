a3e52595ad00330093633b2438559e62399ce932
João Pedro Paiva da Costa Sousa <paivapedro16.pp@gmail.com>
2022-07-25 17:30:56 -0300

    LABETUBE

---
+++ aula-de-tudo/git-1/modulo-3/Reforço-jsx-Labetube/reforco-jsx-labetube/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to LabetubeX</h1>
+        <p>Coming soon </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
