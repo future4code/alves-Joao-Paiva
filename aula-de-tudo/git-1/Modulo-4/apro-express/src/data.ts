3bafcc81539c43c0b0cafc2faea35785d9e07ff1
Joao Pedro <paivapedro16.pp@gmail.com>
2022-08-16 17:22:57 -0300

    Eu me atrapalhei todinho na documentação

---
+++ aula-de-tudo/git-1/Modulo-4/apro-express/src/data.ts
@@ -0,0 +1,29 @@
+// exercicio 2
+type Afazeres = {
+    userId: number;
+    id: number;
+    title: string;
+    completed: boolean;
+  };
+
+  //exercicio 3
+export const Afazeres = [
+    {
+      userId: 1,
+      id: 1,
+      title: "Vai Lavar prato?",
+      completed: false,
+    },
+    {
+      userId: 1,
+      id: 2,
+      title: "Vai fazer almoço?",
+      completed: false,
+    },
+    {
+      userId: 1,
+      id: 3,
+      title: "Vai para Londres ?",
+      completed: false,
+    }
+];
